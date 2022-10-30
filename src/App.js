323cd2a15be222501b86020502c4077ece20bb67
jaschadadap <jdadap25@gmail.com>
2022-10-29 23:48:05 -0600

    create the header component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import Offers from './pages/Offers';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
 import './App.css';
 
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import realtorLogo from '../img/realtor-logo.svg'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+export default function Header() {
+
+    const location = useLocation()
+    const navigate =  useNavigate()
+    const matchPath = (linkRoute) => {
+        if (linkRoute === location.pathname) {
+            return true
+        }
+    }
+
+  return (
+    <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
+        <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
+            <div>
+               <img src={realtorLogo} alt="realtor-logo" className='h-5 cursor-pointer' onClick={() => navigate('/')} />
+            </div>
+            <div>
+                <ul className='flex space-x-10'>
+                    <li onClick={() => navigate('/')} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${matchPath('/') && "text-black border-b-red-500"}`}>Home</li>
+                    <li onClick={() => navigate('/offers')}  className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${matchPath('/offers') && "text-black border-b-red-500"}`}>Offers</li>
+                    <li onClick={() => navigate('/sign-in')}  className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${matchPath('/sign-in') && "text-black border-b-red-500"}`}>Sign In</li>
+                </ul>
+            </div>
+        </header>
+    </div>
+  )
+}
