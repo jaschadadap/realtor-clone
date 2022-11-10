95f66197884ad6384851d39131b5907923b2529b
jaschadadap <jdadap25@gmail.com>
2022-11-10 13:17:15 -0700

    update header menu on authstatechange

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import realtorLogo from '../img/realtor-logo.svg'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useEffect } from 'react'
 
 export default function Header() {
-
+    const [pageState, setPageState] = useState("Sign In")
     const location = useLocation()
     const navigate =  useNavigate()
+    const auth = getAuth()
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setPageState("Profile")
+            } else {
+                setPageState("Sign In")
+            }
+        })
+    }, [auth])
+
     const matchPath = (linkRoute) => {
         if (linkRoute === location.pathname) {
             return true
@@ -22,7 +36,9 @@ export default function Header() {
                 <ul className='flex space-x-10'>
                     <li onClick={() => navigate('/')} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${matchPath('/') && "text-black border-b-red-500"}`}>Home</li>
                     <li onClick={() => navigate('/offers')}  className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${matchPath('/offers') && "text-black border-b-red-500"}`}>Offers</li>
-                    <li onClick={() => navigate('/sign-in')}  className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${matchPath('/sign-in') && "text-black border-b-red-500"}`}>Sign In</li>
+                    <li onClick={() => navigate('/sign-in')}  className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${(matchPath('/sign-in') || matchPath('/profile')) && "text-black border-b-red-500"}`}>
+                        {pageState}
+                    </li>
                 </ul>
             </div>
         </header>
